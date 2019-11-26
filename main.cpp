92a760f99e99873f451b52cb4cbe30b59a91619f
genericsmatrix <kaustubhbawa123@gmail.com>
2019-11-26 12:38:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,17 @@ float vecExp_wrapper(float a);
 float vecMul_wrapper(float a,float b);
 float vecPow_wrapper(float a,float b);
 
-using namespace v8;
-using namespace Nan;
 
-void Add(const FunctionCallbackInfo<Value>& info) {
-  Local<Context> context = info.GetIsolate()->GetCurrentContext();
+void Add(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
 
   if (info.Length() < 3) {
-    ThrowTypeError("Wrong number of arguments");
+    Nan::ThrowTypeError("Wrong number of arguments");
     return;
   }
 
   if (!info[1]->IsNumber() || !info[2]->IsNumber()) {
-    ThrowTypeError("Wrong arguments");
+    Nan::ThrowTypeError("Wrong arguments");
     return;
   }
 
@@ -29,35 +27,35 @@ void Add(const FunctionCallbackInfo<Value>& info) {
   int type_info=  info[0]->NumberValue(context).FromJust();
   if(type_info==0){
         float res=vecAdd_wrapper(arg0,arg1);
-        Local<Number> num = New(res);
+        v8::Local<v8::Number> num = Nan::New(res);
         info.GetReturnValue().Set(num);
   }else if(type_info==1){
         float res=vecSub_wrapper(arg0,arg1);
-        Local<Number> num = New(res);
+        v8::Local<v8::Number> num = Nan::New(res);
         info.GetReturnValue().Set(num);
   }else if(type_info==2){
         float res=vecMul_wrapper(arg0,arg1);
-        Local<Number> num = New(res);
+        v8::Local<v8::Number> num = Nan::New(res);
         info.GetReturnValue().Set(num);
   }else if(type_info==3){
         float res=vecExp_wrapper(arg0);
-        Local<Number> num = New(res);
+        v8::Local<v8::Number> num = Nan::New(res);
         info.GetReturnValue().Set(num);
   }else if(type_info==4){
         float res=vecPow_wrapper(arg0,arg1);
-        Local<Number> num = New(res);
+        v8::Local<v8::Number> num = Nan::New(res);
         info.GetReturnValue().Set(num);
   }else{
-        Local<Number> num = New(-1);
+        v8::Local<v8::Number> num = Nan::New(-1);
         info.GetReturnValue().Set(num);
   }
 }
 
-void Init(Local<Object> exports) {
-  Local<Context> context = exports->CreationContext();
+void Init(v8::Local<v8::Object> exports) {
+  v8::Local<v8::Context> context = exports->CreationContext();
   exports->Set(context,
-               New("math_func").ToLocalChecked(),
-               New<FunctionTemplate>(Add)
+               Nan::New("math_func").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(Add)
                    ->GetFunction(context)
                    .ToLocalChecked());
 }
